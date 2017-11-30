e9f59a60ae7aee6a693d3164b7b48de0a687bd83
enoch-lee <lzsapril666@gmail.com>
2017-11-29 23:00:57 -0600

    remove Pair class

--- src/Pair.java
+++
@@ -1,9 +0,0 @@
-//public class Pair<A, B> {
-//    public A first;
-//    public B second;
-//
-//    public Pair(A first, B second) {
-//        this.first = first;
-//        this.second = second;
-//    }
-//}
